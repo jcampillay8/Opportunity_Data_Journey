a9c55edb450d468d907d00180e7262215d1df7f1
jcampillay <jcampillayworks@gmail.com>
2024-03-28 18:31:50 -0300

    28 marzo 2024 actualizar datos desde usuario empleado

--- apps/request/forms/revision_solicitud.py
+++ apps/request/forms/revision_solicitud.py
@@ -12,7 +12,7 @@ from django_pandas.io import read_frame
 from django.contrib.auth.decorators import login_required
 import plotly.graph_objects as go
 import requests
-
+from django.contrib import messages
 import base64
 import datetime    
 import io
@@ -41,6 +41,14 @@ def serve_layout():
 
         ]),
     dbc.Row([
+            dbc.Col(width=1),
+            dbc.Col((
+            html.Div(id='output_nombre_formulario', style={'fontSize': 30, 'font-weight': 'bold'}),
+            ),width=10),
+            dbc.Col(width=1),
+
+        ]),
+    dbc.Row([
         dbc.Col((html.Div(style={'height': '20px'})),width=1),
         dbc.Col((html.Div(id='output-user')),width=10),
         dbc.Col((html.Div(style={'height': '20px'})),width=1),
@@ -48,67 +56,42 @@ def serve_layout():
     dbc.Row([
         dbc.Col(width=1),
         dbc.Col((
-        dbc.Row([
-        dbc.Col(html.Div([
-            html.Br(),
-            dcc.Markdown(''' ### NOMBRE PRODUCTO: '''),
-        ]), md=12, width=2, lg=2),
-        dbc.Col(html.Div([
-            html.Br(),
-            dcc.Input(
-                id = 'output_nombre_producto',
-                placeholder='Enter a value...',
-                type='text',
-                value='',
-                style={'width': '100%'},
-                readOnly=True  # Make the input read-only
-            ),
-        ], className='pl-0'), md=12, width=6, lg=6),
-        dbc.Col(html.Div([
-            html.Br(),
-            dcc.Markdown(''' ### CANTIDAD: '''),
-        ]), md=12, width=2, lg=2),
-        dbc.Col(html.Div([
-            html.Br(),
-            dcc.Input(
-                id = 'output_cantidad',
-                placeholder='Enter a value...',
-                type='number',
-                value='',
-                style={'width': '100%'},
-                readOnly=True  # Make the input read-only
-            ),
-        ], className='pl-0'), md=12, width=2, lg=2),
-        
-            ])
+            dash_table.DataTable(
+                id='output-table',
+                columns=[
+                    {"name": "ID", "id": "ID"},
+                    {"name": "Nombre Producto", "id": "Nombre_Producto"},
+                    {"name": "Cantidad", "id": "Cantidad"},
+                    {"name": "Descripción Producto", "id": "Descripcion_Producto"}
+                ],
+                data=[],
+                editable=True,  # Hace que la tabla sea editable
+                style_cell={'textAlign': 'left'},
+                style_header={
+                    'backgroundColor': 'rgb(230, 230, 230)',
+                    'fontWeight': 'bold'
+                }
+            )
         ),width=10),
         dbc.Col(width=1),
     ]),
-    html.Div(style={'height': '40px'}),  # Add a space
+    dbc.Row([  # Agrega esta línea
+        dbc.Col(width=1),
+        dbc.Col((dbc.Button('SAVE', id='save-button', style={'display': 'none'})),width=10),  # Agrega esta línea
+        dbc.Col(width=1),
+    ]),  # Agrega esta línea
     dbc.Row([
-        dbc.Col(width=1),
-        dbc.Col((
-        dcc.Markdown(''' ### DESCRIPCIÓN PRODUCTO: '''),
-
-        ),width=10),
-        dbc.Col(width=1),
-
-    ]),
-    dbc.Row([
-        dbc.Col(width=1),
-        dbc.Col((
-            dcc.Textarea(
-            id = 'output_descripcion_producto',
-            placeholder='Describa el ítem...',
-            style={'width': '100%'},
-            readOnly=True  # Make the textarea read-only
-        )
-        ),width=10),
-        dbc.Col(width=1),
-
-    ]),
+            dbc.Col((),width=1),
+            dbc.Col((
+                html.Div(style={'height': '20px'}),
+                html.Div( id='output-message', className="text-center"),
+                html.Div(style={'height': '20px'}),
+            ),width=10),
+            dbc.Col((),width=1),
+        ]),
     html.Div(id='user_id', style={'display': 'none'}),
     html.Div(id='username', style={'display': 'none'}),
+    dcc.ConfirmDialog(id='confirm',message=''),
 
     # Add a Submit button to the layout
     html.Button('Submit', id='submit', n_clicks=0, style={'display': 'none'}),
@@ -121,6 +104,18 @@ def serve_layout():
 app.layout = serve_layout
 
 
+@app.callback(
+    Output('save-button', 'style'),
+    [Input('submit', "n_clicks")],
+)
+def update_button(n_clicks, request):
+    user = request.user
+    if n_clicks is not None:
+        if user.is_superuser or user.is_staff:
+            return {'display': 'block'}
+        else:
+            return {'display': 'none'}
+    return {'display': 'none'}
 
 
 @app.callback(
@@ -139,31 +134,9 @@ def get_user(n_clicks, user_id, username, request):  # Agrega pathname aquí
         return f"El nombre de usuario es {username}, su id es {user_id} y el Id de OC es {ID_OC}"
 
 
-# @app.callback(
-#     [Output('output_nombre_producto', 'value'),
-#      Output('output_cantidad', 'value'),
-#      Output('output_descripcion_producto', 'value')],
-#     [Input('submit', "n_clicks")],
-# )
-# def get_product(n_clicks,request):
-#     ID_OC = request.session.get('id')
-#     if n_clicks is not None:
-#         product = CotizacionRealizada_Productos.objects.filter(ID_OC=ID_OC).first()
-#         if product is not None:
-#             return product.Nombre_Producto, product.Cantidad, product.Descripcion_Producto
-#         else:
-#             return "No se encontró un producto con ese id", "", ""
-#     return "No se encontró un id válido", "", ""
-
-
-
 @app.callback(
-    [Output('output_nombre_producto', 'value'),
-     Output('output_cantidad', 'value'),
-     Output('output_descripcion_producto', 'value'),
-     Output('output_nombre_producto', 'readOnly'),
-     Output('output_cantidad', 'readOnly'),
-     Output('output_descripcion_producto', 'readOnly')],
+    [Output('output-table', 'data'),
+     Output('output-table', 'editable')],
     [Input('submit', "n_clicks")],
 )
 def get_product(n_clicks,request):
@@ -171,13 +144,60 @@ def get_product(n_clicks,request):
     id = request.session.get('id')  # Obtiene el id de la sesión
     user_id = user.id
     if n_clicks is not None:
-        product = CotizacionRealizada_Productos.objects.filter(ID_OC_id=id).first()
+        products = CotizacionRealizada_Productos.objects.filter(ID_OC_id=id)  # Obtiene todos los productos
+        # Crea una lista de diccionarios con los datos de los productos
+        data = [{'ID': product.id, 'Nombre_Producto': product.Nombre_Producto, 'Cantidad': product.Cantidad, 'Descripcion_Producto': product.Descripcion_Producto} for product in products]
         if user.is_superuser or user.is_staff:
-            # Filtra por ID_OC_id en lugar de ID_OC
-            if product is not None:
-                return product.Nombre_Producto, product.Cantidad, product.Descripcion_Producto, False, False, False
-            else:
-                return "No se encontró un producto con ese id", "", "", True, True, True
+            return data, True
         else:
-            return product.Nombre_Producto, product.Cantidad, product.Descripcion_Producto, True, True, True
-    return "No se encontró un id válido", "", "", True, True, True
+            return data, False
+    return [], False
+
+
+
+
+@app.callback(
+    [Output("confirm", "displayed"), Output("confirm", "message")],
+    [Input("save-button", "n_clicks")]
+)
+def open_confirm_dialog(n_clicks):
+    if n_clicks is not None and n_clicks > 0:
+        return True, "¿Está seguro de guardar la información?"
+    return False, ""
+
+
+
+@app.callback(
+    Output('output-message', 'children'),
+    [Input('confirm', 'submit_n_clicks')],
+    [State('output-table', 'data')]
+)
+def update_output(submit_n_clicks, table_data, request):
+    id = request.session.get('id')
+    if submit_n_clicks is not None:
+        try:
+            # Itera sobre cada fila en los datos de la tabla
+            for row in table_data:
+                # Actualiza los datos en la base de datos para cada producto
+                CotizacionRealizada_Productos.objects.filter(ID_OC_id=id, id=row['ID']).update(
+                    Nombre_Producto=row['Nombre_Producto'],
+                    Cantidad=row['Cantidad'],
+                    Descripcion_Producto=row['Descripcion_Producto']
+                )
+
+            # Si la actualización fue exitosa, devuelve un mensaje de éxito
+            return dbc.Alert(
+                [html.I(className="bi bi-check-circle-fill me-2"), "Datos actualizados exitosamente"],
+                color="success",
+                className="d-flex align-items-center",
+            )
+        except Exception as e:
+            # Si ocurrió un error durante la actualización, devuelve un mensaje de error
+            return dbc.Alert(
+                [html.I(className="bi bi-x-octagon-fill me-2"), f'Error actualización datos: {str(e)}'],
+                color="danger",
+                className="d-flex align-items-center",
+            )
+    # Si el usuario hizo clic en "Cancelar", no devuelve nada
+    return None
+
